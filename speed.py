967bb0330d7eba2451c0b5b676b343914f60856b
Pyeon-MinWoo <038apple@naver.com>
2023-05-26 12:55:10 +0900

    lower_color, upper_color 값 조정 및 변경

--- speed.py
+++ speed.py
@@ -2,18 +2,21 @@ import cv2
 import math
 
 cap = cv2.VideoCapture('C:/Users/pyeonmu/Desktop/test/data/111.mp4')
-tracker = cv2.TrackerKCF_create()
+tracker = cv2.TrackerKCF_create() 
 
 # Select initial bounding box for the player
+# 플레이어의 초기 경계 상자 선택
 ret, frame = cap.read()
 bbox = cv2.selectROI(frame, False)
 
 
 
 # Initialize the tracker
+# 추적기 초기화
 tracker.init(frame, bbox)
 
 # Initialize variables for tracking speed
+# 추적 속도에 대한 변수 초기화
 prev_pos = (bbox[0] + bbox[2]/2, bbox[1] + bbox[3]/2)
 prev_time = 0
 
@@ -21,41 +24,52 @@ while True:
     ret, frame = cap.read()
 
     # Update the tracker and get the new bounding box
+    # 트래커를 업데이트하고 새 경계 상자 가져오기
     success, bbox = tracker.update(frame)
 
     if success:
         # Draw the bounding box around the player
+        # 플레이어 주위에 경계 상자를 그립니다.
         (x, y, w, h) = [int(i) for i in bbox]
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         # Calculate the current position of the player
+        # 플레이어의 현재 위치 계산
         current_pos = (x + w/2, y + h/2)
 
         # Calculate the Euclidean distance between the current and previous positions
+        # 현재 위치와 이전 위치 사이의 유클리드 거리 계산
         distance = math.sqrt((current_pos[0] - prev_pos[0])**2 + (current_pos[1] - prev_pos[1])**2)
 
         # Calculate the time elapsed since the previous frame
+        # 이전 프레임 이후 경과된 시간 계산
         current_time = cap.get(cv2.CAP_PROP_POS_MSEC)
         time_elapsed = (current_time - prev_time) / 1000
 
         # Calculate the speed of the player in pixels per second
+        # 플레이어의 속도를 초당 픽셀 단위로 계산합니다.
         speed = distance / time_elapsed
 
         # Display the speed on the frame
+        # 프레임에 속도 표시
         cv2.putText(frame, "Speed: {:.2f} pixels/sec".format(speed), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
         # Update the previous position and time
+        # 이전 위치 및 시간 업데이트
         prev_pos = current_pos
         prev_time = current_time
     else:
         # Player not found, break out of loop
+        # 플레이어를 찾을 수 없습니다. 루프에서 벗어납니다.
         break
 
     # Display the frame with the player bounding box and speed
+    # 플레이어 경계 상자 및 속도로 프레임 표시
     cv2.imshow('Player Tracking', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
 
 cap.release()
 cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -6,16 +6,18 @@ cap = cv2.VideoCapture('C:/Users/pyeonmu/Desktop/test/data/111.mp4')
 
 # Define color range for fireworks (in this example, red and yellow)
 # 불꽃놀이의 색상 범위 정의 (이 예에서, 빨간색과 노란색)
-lower_color = np.array([0, 50, 50])
-upper_color = np.array([30, 255, 255])
+lower_color = np.array([255, 255, 255])
+upper_color = np.array([0, 0, 0])
 
 # Define image processing parameters
+# 이미지 처리 매개변수 정의
 blur_kernel_size = (3, 3)
 edge_threshold1 = 50
 edge_threshold2 = 150
 canny_aperture_size = 3
 
 # Loop over frames in video
+# 비디오의 프레임 오버 루프
 while True:
     # Read frame
     ret, frame = cap.read()
@@ -23,30 +25,37 @@ while True:
         break
     
     # Convert frame to grayscale
+    # 프레임을 그레이스케일로 변환
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Apply Gaussian blur to smooth image
+    # 부드러운 이미지에 가우시안 블러 적용
     blurred = cv2.GaussianBlur(gray, blur_kernel_size, 0)
 
     # Detect edges using Canny edge detection
+    # Canny 에지 감지를 사용하여 에지 감지
     edges = cv2.Canny(blurred, edge_threshold1, edge_threshold2, apertureSize=canny_aperture_size)
 
     # Dilate the edges to thicken the lines
+    # 가장자리를 확장하여 선을 두껍게 만듭니다.
     kernel = np.ones((3, 3), np.uint8)
     dilated_edges = cv2.dilate(edges, kernel, iterations=1)
 
     # Apply color threshold to isolate fireworks
+    # 색상 임계값을 적용하여 불꽃놀이 분리
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color, upper_color)
     fireworks = cv2.bitwise_and(frame, frame, mask=mask)
 
     # Merge the fireworks and edges images
+    # 불꽃놀이와 가장자리 이미지 병합
     merged = cv2.addWeighted(fireworks, 1, cv2.cvtColor(dilated_edges, cv2.COLOR_GRAY2BGR), 1, 0)
 
     # Display original and processed frames
+    # 원본 및 처리된 프레임 표시
     cv2.imshow('Original', frame)
     cv2.imshow('Processed', merged)
-
+    
     # Wait for key press to exit
     if cv2.waitKey(1) == ord('q'):
         break
